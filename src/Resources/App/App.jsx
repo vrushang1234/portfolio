bbc4b12b9316667a55ac1feb40e0ca942c17da95
vrushang1234 <vrushanganand20@gmail.com>
2023-06-06 10:37:19 -0700

    Phone reload

--- src/Resources/App/App.jsx
+++ src/Resources/App/App.jsx
@@ -4,6 +4,16 @@ import Page2 from "../Page2/Page2"
 import Page3 from "../Page3/Page3"
 import Page4 from "../Page4/Page4"
 export default function App(){
+    if (navigator.userAgent.match(/Android/i)
+         || navigator.userAgent.match(/webOS/i)
+         || navigator.userAgent.match(/iPhone/i)
+         || navigator.userAgent.match(/iPad/i)
+         || navigator.userAgent.match(/iPod/i)
+         || navigator.userAgent.match(/BlackBerry/i)
+         || navigator.userAgent.match(/Windows Phone/i)) {
+            alert("Please view on desktop :)");
+            window.location.reload();
+         } 
     return(
         <div className="body">
             <Page1 />
